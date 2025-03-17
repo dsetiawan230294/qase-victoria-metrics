2166dc4758df56b07bb5cab0614f1b681084a612
Didit Setiawan <didit@pintu.co.id>
2025-03-17 18:31:04 +0700

    v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="qase-victoria-metrics",
-    version="0.1.1",
+    version="0.1.3",
     description="A Qase-Pytest plugin to extract test results and push metrics to Victoria Metrics",
     long_description=long_description,
     long_description_content_type="text/markdown",
