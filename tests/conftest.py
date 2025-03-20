33edc7656c6036f4093a5c762775c08f9e509a6a
Didit Setiawan <didit@pintu.co.id>
2025-03-21 04:13:00 +0700

    Implement tied execution with pytest conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,11 +15,17 @@ def pytest_runtest_makereport(item, call):
         item.test_result = rep
 
 
+@pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
     worker_id = os.environ.get("PYTEST_XDIST_WORKER")
 
-    if worker_id:
-        qase_report.save_to_temp_file(worker_id)
-    else:
-        qase_report.load_and_merge_results()
-        qase_report.send_to_victoria_metrics()
+    try:
+        if worker_id:
+            qase_report.save_to_temp_file(worker_id)
+        else:
+            qase_report.load_and_merge_results()
+            qase_report.send_to_victoria_metrics()
+    except Exception as e:
+        session.config.pluginmanager.get_plugin("terminalreporter").write(
+            f"\n[Metrics Report] Error: {e}\n"
+        )
